42790cd89748cf2c80d4758531b5691d72dc076f
MusaAditepe <111117262+MusaAditepe@users.noreply.github.com>
2024-08-05 12:07:09 +0300

    Initial Commit

---
+++ Photorama SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Photorama SwiftUI
+//
+//  Created by Musa Adıtepe on 5.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Photorama SwiftUI/Photorama_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photorama_SwiftUIApp.swift
+//  Photorama SwiftUI
+//
+//  Created by Musa Adıtepe on 5.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Photorama_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
